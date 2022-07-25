2b5deeaf6eb4800812d6e3892ce8382c011e2a73
Abdullah_assoli <abdullahassoli@gmail.com>
2022-07-25 15:51:36 +0200

    push

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,22 +1,36 @@
-import classes from './Auth.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store/index";
+import classes from "./Auth.module.css";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const loginHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+    dispatch(authActions.login());
+  };
   return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
+    <>
+      {isAuthenticated && <div>hi Mr.X</div>}
+      {!isAuthenticated && (
+        <main className={classes.auth}>
+          <section>
+            <form onSubmit={loginHandler}>
+              <div className={classes.control}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" />
+              </div>
+              <div className={classes.control}>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" />
+              </div>
+              <button>Login</button>
+            </form>
+          </section>
+        </main>
+      )}
+    </>
   );
 };
 
